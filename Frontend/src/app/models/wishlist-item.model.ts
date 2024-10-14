5eea67f25e87e3478b778ae26d377e346aec9635
Pooja-Kannan09 <112713822+Pooja-Kannan09@users.noreply.github.com>
2024-10-14 18:32:46 +0530

    Add files via upload

---
+++ Frontend/src/app/models/wishlist-item.model.ts
@@ -0,0 +1,12 @@
+export interface WishlistItem {
+  foodid: number;
+  name: string;
+  caloric: number;
+  type: string;
+  fat: number;
+  carbon: number;
+  protein: number;
+  categoryId: number;
+  image: string;
+  }
+  
